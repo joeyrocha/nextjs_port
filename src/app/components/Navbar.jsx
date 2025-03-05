f3c1084d81d57f47b83c6c71b62a0b9ba3202098
Joe Rocha-Perez <jrochaperez123@gmail.com>
2025-03-04 18:54:17 -0800

    updated exp

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,4 +1,3 @@
-"use client";
 import Link from "next/link";
 import React, { useState } from "react";
 import NavLink from "./NavLink";
@@ -26,10 +25,7 @@ const Navbar = () => {
   return (
     <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
-        <Link
-          href={"/"}
-          className="text-2xl md:text-5xl text-white font-semibold"
-        >
+        <Link href="/" className="text-2xl md:text-5xl text-white font-semibold">
           joeyrocha
         </Link>
         <div className="mobile-menu block md:hidden">
@@ -59,7 +55,7 @@ const Navbar = () => {
           </ul>
         </div>
       </div>
-      {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
+      {navbarOpen && <MenuOverlay links={navLinks} />}
     </nav>
   );
 };
