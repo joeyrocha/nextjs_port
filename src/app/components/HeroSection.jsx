1a6b7a5b619a745bd15fb4cbbce3e31b141c9cbb
Joe Rocha-Perez <jrochaperez123@gmail.com>
2025-07-01 11:05:14 -0700

    added resume

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -52,7 +52,7 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="https://shorturl.at/MibTV"
+              href="https://docs.google.com/document/d/1TkBC35uxyC80cc4p5s9vZXIByjJQHCnX/edit?usp=sharing&ouid=113240927177203808326&rtpof=true&sd=true"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
